ea47103eceb9576d3682119c21d03af189acc5d8
1guankui <73282098+1guankui@users.noreply.github.com>
2021-02-22 11:42:24 +0800

    Update T1.java

--- son1/src/main/java/com/node/T1.java
+++ son1/src/main/java/com/node/T1.java
@@ -2,6 +2,6 @@ package com.node;
 
 public class T1 {
     public static void main(String[] args) {
-        System.out.println("hello world heheheh");
+        System.out.println("hello t2");
     }
 }
